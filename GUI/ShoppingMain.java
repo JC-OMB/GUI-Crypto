29d40dc1b60daebedfa7117eedab4a62638692fb
gisgiselle <brengimolina@gmail.com>
2022-05-16 15:06:22 -0500

    Corrections to GUI
    Added total durations of trip

--- GUI/ShoppingMain.java
+++ GUI/ShoppingMain.java
@@ -3,13 +3,13 @@
 public class ShoppingMain {
     public static void main(String[] args) {
         Catalog list = new Catalog("CS Gift Catalog");
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // From Tlalpan to Coyoacan Center ", 200, " yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center ", 180," yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center ", 90," yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center", 320," yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center ", 230," yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center ", 400," yes"));
-        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 //From Tlalpan to Coyoacan Center ", 35," yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
+        list.add(new Item("Car Model: Toyota  //  Date: May 15  //  Hour: 16:30 // Total duration: 40 mins //  From Tlalpan to Coyoacan Center ", 200, " yes"));
       
 
         ShoppingFrame f = new ShoppingFrame(list);
